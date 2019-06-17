2451ffbe32e7bc987c392d4f757172d8f29ccb1f
Pashio84 <yuhki_yk613st730@yahoo.co.jp>
2019-06-17 17:10:37 +0900

    Update

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-pandoc style.yaml main.md -o result.pdf -F pandoc-crossref -V documentclass=ltjarticle --template=template.tex --pdf-engine=lualatex --toc -N
+pandoc ./config/style.yaml main.md -o result.pdf \
+    -F pandoc-crossref \
+    -M "crossrefYaml=./config/pandoc-crossref.yaml" \
+    -F pandoc-citeproc \
+    -V documentclass=ltjarticle \
+    --listings \
+    --template=./templates/template.tex \
+    --pdf-engine=lualatex \
+    --toc -N
